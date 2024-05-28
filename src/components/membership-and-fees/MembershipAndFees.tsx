459e8e01bfc8b29afe6f0285a78b8b9ed44c87f3
amolg98 <amol.g98@gmail.com>
2024-05-28 01:03:14 -0400

    Update Membership&Fees

--- src/components/membership-and-fees/MembershipAndFees.tsx
+++ src/components/membership-and-fees/MembershipAndFees.tsx
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import WaiverForm from "./WaiverForm";
 import PaymentInfo from "./PaymentInfo";
 import Footer from "../footer/Footer";
 
-class MembershipAndFees extends Component {
-  render() {
-    return (
-      <div className="bg-gray-100 justify-center items-center">
-        <div>
-          <WaiverForm />
-        </div>
-        <div className="bg-white flex-grow flex flex-col justify-center">
-          <PaymentInfo />
-        </div>
-        <div>
-          <Footer />
-        </div>
+const MembershipAndFees = () => {
+  return (
+    <div className="bg-gray-100 justify-center items-center">
+      <div>
+        <WaiverForm />
       </div>
-    );
-  }
-}
+      <div className="bg-white flex-grow flex flex-col justify-center">
+        <PaymentInfo />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default MembershipAndFees;
