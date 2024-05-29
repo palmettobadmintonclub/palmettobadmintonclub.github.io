f9f64f674e3ca402dc0a2602add6678eee1ba937
amolg98 <amol.g98@gmail.com>
2024-05-29 13:07:43 -0400

    Add robots.txt and sitemap.txt

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Home from "./components/home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MembershipAndFees from "./components/membership-and-fees/MembershipAndFees";
 import NavBar from "./components/headerbar/NavBar";
 
 function App() {
   return (
     <div className="text-center w-full">
-      <BrowserRouter>
+      <HashRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/membership-and-fees" element={<MembershipAndFees />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
